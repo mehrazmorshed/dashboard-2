c7a9b97b7a7bd89ba6549299f1f23da695f451ce
Mehraz Morshed <mehrazmorshed@gmail.com>
2023-12-26 22:31:43 +0600

    Update catDeleteController.php

--- controller/catDeleteController.php
+++ controller/catDeleteController.php
@@ -3,4 +3,15 @@
   include_once "../config/dbconnect.php";
 
   $c_id = $_POST['record'];
+  $query = "DELETE FROM category where category_id = '$c_id'";
+
+  $data = mysqli_query($conn, $query);
+
+  if($data){
+    echo "Delete Successful";
+  }
+  else{
+    echo "Delete Failed";
+  }
+
 ?>
